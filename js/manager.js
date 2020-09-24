7de28c6eec54a93ea561048e2b719a418145aa9f
Rahul Singh Dhek <rahulsinghdhek@gmail.com>
2020-09-24 17:26:01 +0530

    Add files via upload

---
+++ js/manager.js
@@ -0,0 +1,276 @@
+var bookShelf1 = {
+	"borntorun":{
+		"imageSource":"images/borntorun.jpg",
+		"tags":["justfinished","mustread","all","nonfiction"],
+		"url":"https://www.amazon.in/gp/product/1861978774/ref=ppx_yo_dt_b_asin_title_o02_s00?ie=UTF8&psc=1"
+	},
+	"coffeeCanInvesting":{
+		"imageSource":"images/coffeeCanInvesting.jpg",
+		"tags":["current"],
+		"url":"https://www.amazon.in/gp/product/067009045X/ref=ppx_yo_dt_b_asin_title_o07_s01?ie=UTF8&psc=1"
+	}
+}
+var bookShelf = {
+	"borntorun":{
+		"imageSource":"images/borntorun.jpg",
+		"tags":["justfinished","mustread","all","nonfiction"],
+		"url":"https://www.amazon.in/gp/product/1861978774/ref=ppx_yo_dt_b_asin_title_o02_s00?ie=UTF8&psc=1"
+	},
+	"coffeeCanInvesting":{
+		"imageSource":"images/coffeeCanInvesting.jpg",
+		"tags":["current"],
+		"url":"https://www.amazon.in/gp/product/067009045X/ref=ppx_yo_dt_b_asin_title_o07_s01?ie=UTF8&psc=1"
+	},
+	"richestManInBabylon":{
+		"imageSource":"images/richestManInBabylon.jpeg",
+		"tags":["justfinished","mustread","all","finance"],
+		"url":"https://www.amazon.in/gp/product/9388144317/ref=ppx_yo_dt_b_asin_title_o03_s04?ie=UTF8&psc=1"
+	},
+	"chasingmonsoon":{
+		"imageSource":"images/chasingmonsoon.jpg",
+		"tags":["unfinished"],
+		"url":"https://www.amazon.in/gp/product/033043313X/ref=ppx_yo_dt_b_asin_title_o00_s00?ie=UTF8&psc=1"
+	},
+	"killamockingbird":{
+		"imageSource":"images/killamockingbird.jpg",
+		"tags":["all","fiction"],
+		"url":"https://www.amazon.in/gp/product/0099549484/ref=ppx_yo_dt_b_asin_title_o01_s00?ie=UTF8&psc=1"
+	},
+	"kiterunner":{
+		"imageSource":"images/kiterunner.jpg",
+		"tags":["all","fiction","mustread"],
+		"url":"https://www.amazon.in/Kite-Runner-Khaled-Hosseini/dp/1408850257/ref=pd_sbs_14_1/258-2149155-5403660?_encoding=UTF8&pd_rd_i=1408850257&pd_rd_r=2d5a4317-87f0-483d-b9ef-d342fbb9f441&pd_rd_w=xBwiB&pd_rd_wg=LinWn&pf_rd_p=00b53f5d-d1f8-4708-89df-2987ccce05ce&pf_rd_r=S1XGPSK8JJTBKS7W4GNF&psc=1&refRID=S1XGPSK8JJTBKS7W4GNF"
+	},
+	"closestfriend":{
+		"imageSource":"images/closestfriend.jpg",
+		"tags":["fiction","all"],
+		"url":"https://www.amazon.in/Your-Closest-Friend-shocking-thriller/dp/1405936657/ref=sr_1_1?crid=19WGN6ND7LB9L&dchild=1&keywords=your+closest+friend&qid=1600091979&s=books&sprefix=your+close%2Cstripbooks%2C350&sr=1-1"
+	},
+	"richdadpoordad":{
+		"imageSource":"images/richdadpoordad.jpg",
+		"tags":["mustread","all","finance"],
+		"url":"https://www.amazon.in/gp/product/1612680011/ref=ppx_yo_dt_b_asin_title_o05_s01?ie=UTF8&psc=1"
+	},
+	"dreamingreality":{
+		"imageSource":"images/dreamingreality.jpg",
+		"tags":["all","fiction"],
+		"url":"https://www.amazon.in/gp/product/B079HWCNNC/ref=ppx_yo_dt_b_asin_title_o04_s00?ie=UTF8&psc=1"
+	},
+	"valueinvesting":{
+		"imageSource":"images/valueinvesting.jpg",
+		"tags":["all","finance"],
+		"url":"https://www.amazon.in/gp/product/0070077630/ref=ppx_yo_dt_b_asin_title_o00_s00?ie=UTF8&psc=1"
+	},
+	"whenroadbeckon":{
+		"imageSource":"images/whenroadbeckon.jpg",
+		"tags":["all","nonfiction"],
+		"url":"https://www.amazon.in/gp/product/9352352440/ref=ppx_yo_dt_b_asin_title_o08_s00?ie=UTF8&psc=1"
+	},
+	"itoohadadream":{
+		"imageSource":"images/itoohadadream.jpg",
+		"tags":["all","nonfiction","mustread"],
+		"url":""
+	},
+	"fivepointsomeone":{
+		"imageSource":"images/fivepointsomeone.jpg",
+		"tags":["all","fiction"],
+		"url":"https://www.amazon.in/gp/product/B013VO02QI/ref=dbs_a_def_rwt_bibl_vppi_i6"
+	},
+	"girlontrain":{
+		"imageSource":"images/girlontrain.jpg",
+		"tags":["all","fiction"],
+		"url":"https://www.amazon.in/gp/product/0552779776/ref=ppx_yo_dt_b_asin_title_o00_s00?ie=UTF8&psc=1"
+	},
+	"howimade2million":{
+		"imageSource":"images/howimade2million.jpg",
+		"tags":["all","finance"],
+		"url":"https://www.amazon.in/Made-Stock-Market-Hardcover-Library/dp/9389440211/ref=sr_1_1?crid=DQG4RTWMJVEF&dchild=1&keywords=how+i+made+2+million+in+the+stock+market&qid=1600268969&sprefix=how+i+made+2+mi%2Caps%2C347&sr=8-1"
+	},
+	"intelligentInvestor":{
+		"imageSource":"images/intelligentInvestor.jpg",
+		"tags":["unfinished"],
+		"url":"https://www.amazon.in/gp/product/0062312685/ref=ppx_yo_dt_b_asin_title_o09_s00?ie=UTF8&psc=1"
+	},
+	"keepofgrass":{
+		"imageSource":"images/keepofgrass.jpg",
+		"tags":["upcoming"],
+		"url":"https://www.amazon.in/gp/product/9352640284/ref=ppx_yo_dt_b_asin_title_o02_s00?ie=UTF8&psc=1"
+	},
+	"mountainsechoed":{
+		"imageSource":"images/mountainsechoed.jpeg",
+		"tags":["unfinished"],
+		"url":"https://www.amazon.in/Mountains-Echoed-Khaled-Hosseni/dp/9384898082/ref=pd_bxgy_img_3/258-2149155-5403660?_encoding=UTF8&pd_rd_i=9384898082&pd_rd_r=a586fc79-2c67-49dc-871b-6c557d45345d&pd_rd_w=AhL2X&pd_rd_wg=wNe3u&pf_rd_p=3e9f62d7-4196-4823-b41a-9bba671558ea&pf_rd_r=GVXN03DJ240RAP1NGTX9&psc=1&refRID=GVXN03DJ240RAP1NGTX9"
+	},
+	"nightatcallcenter":{
+		"imageSource":"images/nightatcallcenter.jpg",
+		"tags":["all","fiction"],
+		"url":"https://www.amazon.in/gp/product/B013VO02NQ/ref=dbs_a_def_rwt_hsch_vapi_tkin_p1_i5"
+	},
+	"paramveer":{
+		"imageSource":"images/paramveer.jpg",
+		"tags":["all","nonfiction"],
+		"url":"https://www.amazon.in/gp/product/0143422359/ref=ppx_yo_dt_b_asin_title_o01_s00?ie=UTF8&psc=1"
+	},
+	"roadtomussorie":{
+		"imageSource":"images/roadtomussorie.jpg",
+		"tags":["all","fiction"],
+		"url":"https://www.amazon.in/gp/product/812910699X/ref=ppx_yo_dt_b_asin_title_o05_s00?ie=UTF8&psc=1"
+	},
+	"splendidsun":{
+		"imageSource":"images/splendidsun.jpg",
+		"tags":["all","fiction"],
+		"url":"https://www.amazon.in/gp/product/1408844443/ref=ppx_yo_dt_b_asin_title_o04_s00?ie=UTF8&psc=1"
+	},
+	"thinkngrow":{
+		"imageSource":"images/thinkngrow.jpg",
+		"tags":["unfinished"],
+		"url":"https://www.amazon.in/gp/product/8192910911/ref=ppx_yo_dt_b_asin_title_o05_s01?ie=UTF8&psc=1"
+	},
+	"travelwithcharley":{
+		"imageSource":"images/travelwithcharley.jpg",
+		"tags":["unfinished"],
+		"url":"https://www.amazon.in/gp/product/0141186100/ref=ppx_yo_dt_b_asin_title_o08_s00?ie=UTF8&psc=1"
+	},
+	"shoedog":{
+		"imageSource":"images/shoedog.jpg",
+		"tags":["upcoming"],
+		"url":"https://www.amazon.in/gp/product/1471146723/ref=ppx_yo_dt_b_asin_title_o03_s00?ie=UTF8&psc=1"
+	},
+	"themostimportantthing":{
+		"imageSource":"images/themostimportantthing.jpg",
+		"tags":["upcoming"],
+		"url":"https://www.amazon.in/gp/product/9353022797/ref=ppx_yo_dt_b_asin_title_o02_s00?ie=UTF8&psc=1"
+	},
+	"atomichabit":{
+		"imageSource":"images/atomichabit.jpg",
+		"tags":["current"],
+		"url":"https://www.amazon.in/gp/product/1847941834/ref=ppx_yo_dt_b_asin_title_o07_s01?ie=UTF8&psc=1"
+	},
+	"2states":{
+		"imageSource":"images/2states.jpg",
+		"tags":["fiction","all"],
+		"url":"https://www.amazon.in/gp/product/B013VO02ZO/ref=dbs_a_def_rwt_bibl_vppi_i10"
+	},
+	"3mistakes":{
+		"imageSource":"images/3mistakes.jpg",
+		"tags":["fiction","all"],
+		"url":"https://www.amazon.in/gp/product/8129135515/ref=dbs_a_def_rwt_hsch_vapi_tu00_p1_i3"
+	},
+	"alchemist":{
+		"imageSource":"images/alchemist.jpg",
+		"tags":["selfimprovement","all"],
+		"url":"https://www.amazon.in/gp/product/8172234988/ref=dbs_a_def_rwt_hsch_vapi_tu00_p1_i0"
+	},
+	
+}
+
+
+
+function updateBackgroundColors(tabs,tabid){
+	for (i = 0 ; i< tabs.length;i++){
+		if(tabs[i]!==tabid){
+		  document.getElementById(tabs[i]).style.backgroundColor = "white";
+		}else{
+		  document.getElementById(tabs[i]).style.backgroundColor = "#c0dde0";
+		}
+	} 
+}
+function updateTabBackgroundColors(tabid,isSubTab){
+	var tabs= ["currenttab","justfinishedtab","upcomingtab","completedtab","unfinishedtab","mustreadtab"];
+	var subTabs = ["alltab","financetab","fictiontab","nonfictiontab","selfimprovementtab"]; 
+	if(isSubTab){
+		updateBackgroundColors(subTabs,tabid);
+		return;
+	}
+	updateBackgroundColors(tabs,tabid);
+	return;
+}
+
+
+function bookShelf(username, password){
+	var x = document.getElementsByClassName("tabName");
+	for (i = 0; i < x.length; i++) {
+		x[i].style.display = "none";  
+	}
+	document.getElementById("current").style.display = "block"; 
+	hideCompletedReads();
+}
+
+function updateBookShelf(){
+	var imageSource = null;
+	var bookUrl = null;
+	var bookTags = null;
+	var mainDiv = "";
+	for (var bookName in bookShelf) {
+		console.log(bookName);
+		imageSource = bookShelf[bookName]["imageSource"]
+		bookTags = bookShelf[bookName]["tags"]
+		bookUrl = bookShelf[bookName]["url"]
+		console.log(bookTags);
+		for ( i=0; i< bookTags.length;i++){
+			console.log(bookTags[i]);
+			mainDiv += '<div class="w3-third w3-container w3-margin-bottom"><a href="'+bookUrl+'" ><img src="'+imageSource+'" style="width:250px;height: 350px;" class="w3-hover-opacity" >'
+			mainDiv += '<div class="w3-container w3-white"></div></a></div>'
+			mainDiv += '</div>'
+			document.getElementById(bookTags[i]).insertAdjacentHTML('beforeend', mainDiv)
+			mainDiv="";
+		}	
+	}
+}
+
+function openTab(evt,tabName) {
+  updateTabBackgroundColors(evt.srcElement.id,false); 
+  var i;
+  var x = document.getElementsByClassName("tabName");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  document.getElementById(tabName).style.display = "block"; 
+  if(tabName!="completed"){
+    hideCompletedReads()
+  }else{
+
+  }
+}
+
+function BookCompleted(tabName,subtabId){
+  var i;
+  var x = document.getElementsByClassName("completedTab");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  document.getElementById(tabName).style.display = "block"; 
+  updateTabBackgroundColors(subtabId,true);
+}
+
+function init(){
+  updateBookShelf();	
+  updateTabBackgroundColors("currenttab",false);
+  updateTabBackgroundColors(null,true);
+
+  var x = document.getElementsByClassName("tabName");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  document.getElementById("current").style.display = "block"; 
+  hideCompletedReads();
+}
+
+function hideCompletedReads(){
+  var x = document.getElementsByClassName("completedTab");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  var x = document.getElementById("completed");
+  x.style.display = "none"
+}
+
+function w3_open() {
+    document.getElementById("mySidebar").style.display = "block";
+    document.getElementById("myOverlay").style.display = "block";
+}
+ 
+function w3_close() {
+    document.getElementById("mySidebar").style.display = "none";
+    document.getElementById("myOverlay").style.display = "none";
+}
