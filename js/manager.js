9cf1448c5e65f690cc095fde67da9f65cdf87cfb
Rahul Singh Dhek <rahulsinghdhek@gmail.com>
2023-03-13 18:58:18 +0530

    Update manager.js

--- js/manager.js
+++ js/manager.js
@@ -346,13 +346,23 @@ var bookShelf = {
 	},
 	"handfulofnuts":{
 		"imageSource":"images/handfulofnuts.jpg",
-		"tags":["current"],
+		"tags":["2023","all","fiction"],
 		"url":"https://www.amazon.in/Handful-Nuts-Ruskin-Bond/dp/0143067400"
 	},
 	"couragetobedisliked":{
 		"imageSource":"images/couragetobedisliked.jpg",
 		"tags":["current"],
 		"url":"https://www.amazon.in/Courage-Disliked-Ichiro-Kishimi-Fumitake/dp/1760630721/ref=sr_1_1_sspa?crid=2YS948N16VWIO&keywords=courage+to+be+disliked+book&qid=1667801971&qu=eyJxc2MiOiIxLjc2IiwicXNhIjoiMS4wNCIsInFzcCI6IjAuNzEifQ%3D%3D&s=books&sprefix=courage%2Cstripbooks%2C369&sr=1-1-spons&psc=1"
+	},
+	"100bagger":{
+		"imageSource":"images/100bagger.jpeg",
+		"tags":["2023","all","finance","mustread"],
+		"url":"https://www.amazon.in/100-Baggers-Stocks-100-1/dp/9391560555/ref=sr_1_1?keywords=100+bagger&qid=1678713802&sr=8-1"
+	},
+	"commonstocks":{
+		"imageSource":"images/commonstocks.jpg",
+		"tags":["current"],
+		"url":"https://www.amazon.in/Common-stocks-uncommon-profits-writings/dp/B0BVWDS8ZH/ref=tmm_pap_swatch_0?_encoding=UTF8&qid=1678713946&sr=8-1-spons"
 	}
 }
 
@@ -368,7 +378,7 @@ function updateBackgroundColors(tabs,tabid){
 	} 
 }
 function updateTabBackgroundColors(tabid,isSubTab){
-	var tabs= ["currenttab","justfinishedtab","upcomingtab","completedtab","unfinishedtab","mustreadtab","2021tab", "2022tab"];
+	var tabs= ["currenttab","justfinishedtab","upcomingtab","completedtab","unfinishedtab","mustreadtab","2021tab", "2022tab", "2023tab"];
 	var subTabs = ["alltab","financetab","fictiontab","nonfictiontab","selfimprovementtab"]; 
 	if(isSubTab){
 		updateBackgroundColors(subTabs,tabid);
